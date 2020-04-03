2642ade2a4c532938b057f4121df6478452f869a
lekomtsev <mike@jsmaster.ru>
2020-04-03 13:57:28 +0400

    Создание и обработка заказов - срередина (нужна доработка)

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,62 +1,75 @@
 <template>
     <v-container>
-        <h1 class="text--secondary">Заказы</h1>
-        <v-row :justify="'center'">
-            <v-col md="6">
-
-                <v-list subheader two-line flat>
-                    <v-list-item
+        <v-layout v-if="loading">
+            <div style="height: 80vh" class="d-flex justify-center align-center">
+                <v-progress-circular
+                    indeterminate
+                    :size="90"
+                    :width="3"
+                    color="purple"
+                ></v-progress-circular>
+            </div>
+        </v-layout>
+        <v-layout v-else-if="!loading && orders.length !== 0">
+            <h1 class="text--secondary">Заказы</h1>
+            <v-row :justify="'center'">
+                <v-col md="6">
+                    <v-card subheader two-line text
                         v-for="(order, i) in orders"
-                        :key="i"
-                    >
-                        <v-list-item-action>
-                            <v-checkbox
-                                :input-value="order.done"
-                                color="success"
-                                @change="markDone(order)"
-                            ></v-checkbox>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title>{{ order.name }}</v-list-item-title>
-                            <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
-                        </v-list-item-content>
-                        <v-list-item-action>
-                            <v-btn
-                                :to="'/ad/' + order.adId"
-                                class="primary">Открыть</v-btn>
-                        </v-list-item-action>
-                    </v-list-item>
-
-                </v-list>
-            </v-col>
-        </v-row>
+                        :key="i">
+                        <v-list-item class="mb-3">
+                            <v-list-item-action>
+                                <v-checkbox
+                                    :input-value="order.done"
+                                    color="success"
+                                    @change="markDone(order)"
+                                ></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ order.name }}</v-list-item-title>
+                                <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                            </v-list-item-content>
+                            <v-list-item-action>
+                                <v-btn
+                                    :to="'/ad/' + order.adId"
+                                    class="primary">Открыть</v-btn>
+                            </v-list-item-action>
+                        </v-list-item>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-layout>
+        <v-layout v-else>
+            <p>У вас нет активных заказов!</p>
+        </v-layout>
     </v-container>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        active: '',
-        toggle: true,
-        settings: '',
-        orders: [
-          {
-            id: '123',
-            name: 'Mikle',
-            phone: '+7 912 455 64 87',
-            adId: '7897879',
-            done: false
-          }
-        ]
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+      },
+      orders () {
+        return this.$store.getters.orders
       }
     },
 
     methods: {
       markDone (order) {
-        order.done = true
-        console.log(order)
+        this.$store.dispatch('markOrderDone', order.id)
+          .then(() => {
+            order.done = true
+          })
+          .then(() => {})
+
+        console.log(order, 'order - markDone')
       }
+    },
+
+    created () {
+      this.$store.dispatch('fetchOrders')
     }
   }
 </script>
--- src/store/orders.js
+++ src/store/orders.js
@@ -1,21 +1,89 @@
+import * as firebase from 'firebase';
+
+class Order {
+  constructor (name, phone, adId, done = false, id = null) {
+    this.name = name,
+    this.phone = phone,
+    this.adId = adId,
+    this.done = done,
+    this.id = id
+  }
+}
+
 export default {
   state: {
     orders: []
   },
   mutations: {
-    createOrder (state, payload) {
-      state.orders.push(payload)
+    loadOrders (state, payload) {
+      state.orders = payload
     }
   },
   actions: {
-    async createOrder () {
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve()
-          console.log(resolve, reject)
-        }, 4000)
-      })
+    async createOrder ({ commit }, { name, phone, adId, ownerId }) {
+      const order = new Order(name, phone, adId)
+      commit('clearError')
+
+      try {
+        await firebase.database().ref(`/users/${ownerId}/orders`).push(order)
+      } catch (error) {
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async fetchOrders ({ commit, getters }) {
+      commit('setLoading', true)
+      commit('clearError')
+
+      const resultOrders = []
+
+      try {
+        const firebaseDataUsers = await firebase.database().ref(`/users/${getters.user.id}/orders`).once('value')
+        const orders = firebaseDataUsers.val()
+
+        console.log(orders)
+
+        Object.keys(orders).forEach((key) => {
+          const ord = orders[key]
+          resultOrders.push(
+            new Order(ord.name, ord.phone, ord.adId, ord.done)
+          )
+        })
+        console.log(resultOrders, '<--- resultOrders')
+
+        commit('loadOrders', resultOrders)
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async markOrderDone({ commit, getters }, payload) {
+      commit('clearError')
+
+      console.log(getters.user.id)
+      console.log(payload)
+
+      try {
+        await firebase.database().ref(`/users/${getters.user.id}/orders`).child(payload).update({
+          done: true
+        })
+      } catch (error) {
+        commit('setError', error.message)
+        throw error
+      }
     }
   },
-  getters: {}
+  getters: {
+    doneOrders (state) {
+      return state.orders.filter(o => o.done)
+    },
+    undoneOrders (state) {
+      return state.orders.filter(o => !o.done)
+    },
+    orders (state, getters) {
+      return getters.undoneOrders.concat(getters.doneOrders)
+    }
+  }
 }
